dc7a9b0271371687dc33db53c92bd0941a05e4eb
Sanja Bhargav <sanja@example.com>
2026-02-14 11:35:53 +0530

    Fix routing basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <HashRouter basename="/Portfolio">
     <App />
   </HashRouter>
 );
