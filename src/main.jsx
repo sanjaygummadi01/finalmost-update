0499ae5ff79899beebe535d003b27dbdcf5cdc24
Sanja Bhargav <sanja@example.com>
2026-02-14 11:50:37 +0530

    Remove router for SPA scrolling site

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter basename="/Portfolio">
+  <HashRouter>
     <App />
   </HashRouter>
 );
